b86f1d98f481c480317bfdaf630305d42f3b78c1
Ruta Reisoglu <rutareiso@protonmail.com>
2022-04-27 10:56:17 -0400

    Add and style buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import About from "./components/About/About";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 // import "./index.css";
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: {
+          600: "#DD1155",
+        },
+      },
+    },
   },
   plugins: [],
 };
