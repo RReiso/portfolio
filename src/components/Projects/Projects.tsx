68e7b75cbf55314b8cf66ea79d1295eb0ea100d7
Ruta Reisoglu <rutareiso@protonmail.com>
2022-05-04 16:59:11 -0400

    Remove unused variable

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Particles from "react-tsparticles";
 import projects from "../../projects/projects";
 import Categories from "../Categories/Categories";
 
